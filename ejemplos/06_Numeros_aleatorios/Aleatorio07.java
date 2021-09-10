f7577223def751b9805cd11bfafc2bb524bb7fa3
libreriavict <90427802+libreriavict@users.noreply.github.com>
2021-09-09 19:12:03 -0500

    Update Aleatorio07.java
    No se aplica el bucle for, solo el switch.

--- ejemplos/06_Numeros_aleatorios/Aleatorio07.java
+++ ejemplos/06_Numeros_aleatorios/Aleatorio07.java
@@ -7,33 +7,35 @@ public class Aleatorio07 {
   public static void main(String[] args) {
     System.out.println("Muestra un día de la semana al azar:");
     
-    for (int i = 0; i < 20; i++) {
-      int  dia = (int)(Math.random()*7) + 1; // genera un número aleatorio
-                                             // entre el 1 y el 7
-      switch(dia) {
-        case 1:
-          System.out.println("lunes ");
-          break;
-        case 2:
-          System.out.println("martes ");
-          break;
-        case 3:
-          System.out.println("miércoles ");
-          break;
-        case 4:
-          System.out.println("jueves ");
-          break;
-        case 5:
-          System.out.println("viernes ");
-          break;
-        case 6:
-          System.out.println("sábado ");
-          break;
-        case 7:
-          System.out.println("domingo ");
-          break;
-        default:
-      }
-    }
+System.out.println("Muestra un dia de la semana al azar");
+
+        int dia = (int) (Math.random() * 7 + 1);
+        
+        switch (dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miercoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sabado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                break;
+
+        }
   }
 }
