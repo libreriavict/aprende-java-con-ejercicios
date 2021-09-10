3dd506c4ec3723a965e7168de44428235b4f956a
libreriavict <90427802+libreriavict@users.noreply.github.com>
2021-09-09 19:35:43 -0500

    Update Array02.java
    Falto un salto de linea.

--- ejemplos/07_Arrays/7.1_Arrays_de_una_dimension/Array02.java
+++ ejemplos/07_Arrays/7.1_Arrays_de_una_dimension/Array02.java
@@ -18,7 +18,7 @@ public class Array02 {
     System.out.println("El array x tiene 5 elementos ¿cuál de ellos quiere ver?");
     System.out.print("Introduzca un número del 0 al 4: ");
     int indice = Integer.parseInt(System.console().readLine());
-    System.out.print("El elemento que se encuentra en la posición " + indice);
+    System.out.print("\nEl elemento que se encuentra en la posición " + indice);
     System.out.println(" es el " + x[indice]);
   }
 }
